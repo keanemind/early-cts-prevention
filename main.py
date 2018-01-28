4622b14319f8ecdedb1755682c856446d589783c
Codetron <balakumaran55@gmail.com>
2018-01-28 05:19:33 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import csv
+import os
 
 print("Connect the Myo armband and sync. \n"
       "Instructions:"
@@ -12,15 +13,24 @@ print("Connect the Myo armband and sync. \n"
 input()
 subprocess.run("myo-data-capture.cpp")
 
-with open("emg-1517131803.csv") as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=",")
-    next(readCSV)
-    maximum = 0
 
-    for row in readCSV:
-        print(type(row))
-        for i in range(1, 9):
-            if int(row[i]) > maximum:
-                maximum = int(row[i])
+def find(name, path):
+  for root, dirs, files in os.walk(path):
+    for file in files:
+      if name in file[0:3]:
+        return os.path.join(root, file)
+
+emgPath = find("emg", "/Users/Bala/Desktop/dataset")
+
+with open (emgPath) as csvfile:
+  readCSV = csv.reader(csvfile, delimiter=",")
+  next(readCSV)
+  max = 0
+  for row in readCSV:
+    for i in range(1, 9):
+      if int(row[i]) > max:
+        max = int(row[i])     
 
 print(max)
+
+os.remove(emgPath)
