426ad454d4af39f6443c860a7c15ad598b0ac61f
keanemind <keanemind@gmail.com>
2018-01-28 05:09:37 -0600

    Linter edits

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Collect, analyze, and display EMG data."""
 
 import subprocess
-import csv 
+import csv
 
 print("Connect the Myo armband and sync. When you are ready, press any key.")
 input()
@@ -10,12 +10,12 @@ subprocess.run("myo-data-capture.cpp")
 with open("emg-1517131803.csv") as csvfile:
     readCSV = csv.reader(csvfile, delimiter=",")
     next(readCSV)
-    max = 0
+    maximum = 0
 
     for row in readCSV:
         print(type(row))
         for i in range(1, 9):
-            if int(row[i]) > max:
-                max = int(row[i])
+            if int(row[i]) > maximum:
+                maximum = int(row[i])
 
 print(max)
