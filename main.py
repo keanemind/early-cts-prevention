509a1bfadc44dc4fc2aa78266bac8a72a6676c6c
keanemind <keanemind@gmail.com>
2018-01-28 05:08:26 -0600

    Replace tabs with spaces

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ input()
 subprocess.run("myo-data-capture.cpp")
 
 with open("emg-1517131803.csv") as csvfile:
-	readCSV = csv.reader(csvfile, delimiter=",")
-	next(readCSV)
-	max = 0
+    readCSV = csv.reader(csvfile, delimiter=",")
+    next(readCSV)
+    max = 0
 
-	for row in readCSV:
-		print(type(row))
-		for i in range(1, 9):
-			if int(row[i]) > max:
-				max = int(row[i])			
+    for row in readCSV:
+        print(type(row))
+        for i in range(1, 9):
+            if int(row[i]) > max:
+                max = int(row[i])
 
 print(max)
